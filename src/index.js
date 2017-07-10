504bcded236dcc7c0a5a1eb12d9b80809c01ad2d
chenkecheng <chenkecheng@chenkechengdeMac-mini.local>
2017-07-10 19:51:42 +0800

    commit all

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './style/index.css'
+import App from './pages/app/app.vue'
+import Vue from 'vue'
+new Vue({
+    el: '#app',
+    render: h => h(App)
+})
---
+++ src/pages/app/app.vue
@@ -0,0 +1,13 @@
+<template>
+<div>Hello VUE</div>
+</template>
+<script>
+export default{
+    name:'app',
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
